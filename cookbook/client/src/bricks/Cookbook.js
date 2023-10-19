aae232feea6944e8f822ecb9e1db91033a8713d1
DominikRusnak <dominik.rusnak@gmail.com>
2023-10-19 23:46:21 +0200

    Druhá časť

---
+++ cookbook/client/src/bricks/Cookbook.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../css/cookbook.module.css";
+
+function CookbookInfo(props) {
+    return (
+        <span className={styles.cookbookHeader}>
+            {props.cookbook.name}
+        </span>
+    );
+}
+
+export default CookbookInfo
