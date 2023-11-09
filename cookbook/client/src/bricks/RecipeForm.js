f5de88fb98e847e6a91ae5b3644b104dc2a46bbc
DominikRusnak <dominik.rusnak@gmail.com>
2023-11-09 01:23:56 +0100

    Dokončenie 6. bloku

--- cookbook/client/src/bricks/RecipeForm.js
+++ cookbook/client/src/bricks/RecipeForm.js
@@ -16,11 +16,27 @@ function RecipeForm({ ingredients }) {
     const [formData, setFormData] = useState({
         name: "",
         procedure: "",
-        ingredientList: [{
+        ingredient_1: {
             ingredientId: "",
-            ingredientAmount: null,
+            ingredientAmount: 0,
             ingredientUnit: "",
-        }],
+        },
+        ingredient_2: {
+            ingredientId: "",
+            ingredientAmount: 0,
+            ingredientUnit: "",
+        },
+        ingredient_3: {
+            ingredientId: "",
+            ingredientAmount: 0,
+            ingredientUnit: "",
+        },
+        ingredient_4: {
+            ingredientId: "",
+            ingredientAmount: 0,
+            ingredientUnit: "",
+        },
+
     });
 
     const setField = (name, val) => {
@@ -31,7 +47,32 @@ function RecipeForm({ ingredients }) {
         });
     };
 
+    const setIngredientId = (name, val) => {
+        return setFormData((formData) => {
+            const newData = { ...formData };
+            const ingredientInfo = newData[name];
+            ingredientInfo.ingredientId = val;
+            return newData;
+        });
+    };
 
+    const setIngredientAmount = (name, val) => {
+        return setFormData((formData) => {
+            const newData = { ...formData };
+            const ingredientInfo = newData[name];
+            ingredientInfo.ingredientAmount = val;
+            return newData;
+        });
+    };
+
+    const setIngredientUnit = (name, val) => {
+        return setFormData((formData) => {
+            const newData = { ...formData };
+            const ingredientInfo = newData[name];
+            ingredientInfo.ingredientUnit = val;
+            return newData;
+        });
+    };
     
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -58,37 +99,173 @@ function RecipeForm({ ingredients }) {
                         type="text"
                         value={formData.name}
                         onChange={(e) => setField("name", e.target.value)}
+                        required
                     />
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                     <Form.Label>Postup</Form.Label>
+                    <div>
                     <Form.Control
-                        type="text"
+                        as="textarea" rows={6}
                         value={formData.procedure}
                         onChange={(e) => setField("procedure", e.target.value)}
-                    />
+                        required
+                        className="mb-3"
+                    /></div>
                 </Form.Group>
                 
                 <Row>
                     <Form.Group as={Col} className="mb-3">
                         <Form.Label>Ingredience</Form.Label>
                             <Form.Select
-                                value={formData.ingredientList}
-                                onChange={(e) => setField("ingredientList.ingredientId", parseInt(e.target.value))}
+                                type="text"
+                                defaultValue="Zvol ingredienci"
+                                onChange={(e) => setIngredientId("ingredient_1", e.target.value)}
                                 required
                             >
-                            <option value="" disabled>Zvol ingredienci</option>
+                                
+                            <option defaultValue>Zvol ingredienci</option>
                             {ingredients.map((ingredient) => (
-                                <option key={ingredient.id}>{ingredient.name}</option>
+                                <option key={ingredient.id} value={ingredient.id}>{ingredient.name}</option>
                             ))}
                             </Form.Select>
+                    </Form.Group>
+                    <Form.Group as={Col} className="mb-3">
+                        <Form.Label>Počet</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={formData.ingredient_1.ingredientAmount}
+                            onChange={(e) => setIngredientAmount("ingredient_1", e.target.value)}
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col} className="mb-3">
+                        <Form.Label>Jednotka</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={formData.ingredient_1.ingredientUnit}
+                            onChange={(e) => setIngredientUnit("ingredient_1", e.target.value)}
+                            required
+                        />
+                    </Form.Group>
+                </Row>
+                <Row>
+                    <Form.Group as={Col} className="mb-3">
+                        <Form.Label>Ingredience</Form.Label>
+                            <Form.Select
+                                type="text"
+                                defaultValue="Zvol ingredienci"
+                                onChange={(e) => setIngredientId("ingredient_2", e.target.value)}
+                                required
+                            >
+                                
+                            <option defaultValue>Zvol ingredienci</option>
+                            {ingredients.map((ingredient) => (
+                                <option key={ingredient.id} value={ingredient.id}>{ingredient.name}</option>
+                            ))}
+                            </Form.Select>
+                    </Form.Group>
+                    <Form.Group as={Col} className="mb-3">
+                        <Form.Label>Počet</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={formData.ingredient_2.ingredientAmount}
+                            onChange={(e) => setIngredientAmount("ingredient_2", e.target.value)}
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col} className="mb-3">
+                        <Form.Label>Jednotka</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={formData.ingredient_2.ingredientUnit}
+                            onChange={(e) => setIngredientUnit("ingredient_2", e.target.value)}
+                            required
+                        />
+                    </Form.Group>
+                </Row>
+                <Row>
+                    <Form.Group as={Col} className="mb-3">
+                        <Form.Label>Ingredience</Form.Label>
+                            <Form.Select
+                                type="text"
+                                defaultValue="Zvol ingredienci"
+                                onChange={(e) => setIngredientId("ingredient_3", e.target.value)}
+                                required
+                            >
+                                
+                            <option defaultValue>Zvol ingredienci</option>
+                            {ingredients.map((ingredient) => (
+                                <option key={ingredient.id} value={ingredient.id}>{ingredient.name}</option>
+                            ))}
+                            </Form.Select>
+                    </Form.Group>
+                    <Form.Group as={Col} className="mb-3">
+                        <Form.Label>Počet</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={formData.ingredient_3.ingredientAmount}
+                            onChange={(e) => setIngredientAmount("ingredient_3", e.target.value)}
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col} className="mb-3">
+                        <Form.Label>Jednotka</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={formData.ingredient_3.ingredientUnit}
+                            onChange={(e) => setIngredientUnit("ingredient_3", e.target.value)}
+                            required
+                        />
+                    </Form.Group>
+                </Row>
+                <Row>
+                    <Form.Group as={Col} className="mb-3">
+                        <Form.Label>Ingredience</Form.Label>
+                            <Form.Select
+                                type="text"
+                                defaultValue="Zvol ingredienci"
+                                onChange={(e) => setIngredientId("ingredient_4", e.target.value)}
+                                required
+                            >
+                                
+                            <option defaultValue>Zvol ingredienci</option>
+                            {ingredients.map((ingredient) => (
+                                <option key={ingredient.id} value={ingredient.id}>{ingredient.name}</option>
+                            ))}
+                            </Form.Select>
+                    </Form.Group>
+                    <Form.Group as={Col} className="mb-3">
+                        <Form.Label>Počet</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={formData.ingredient_4.ingredientAmount}
+                            onChange={(e) => setIngredientAmount("ingredient_4", e.target.value)}
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col} className="mb-3">
+                        <Form.Label>Jednotka</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={formData.ingredient_4.ingredientUnit}
+                            onChange={(e) => setIngredientUnit("ingredient_4", e.target.value)}
+                            required
+                        />
                     </Form.Group>
                 </Row>
 
                 </Modal.Body>
                 <Modal.Footer>
-                                
+                    <div className="d-flex flex-row gap-2">
+                        <Button variant="secondary" onClick={handleCloseModal}>
+                            Zavřít
+                        </Button>
+                        <Button variant="primary" type="submit">
+                            Vytvořit
+                        </Button>
+                    </div>                
                 </Modal.Footer>
             </Form>
         </Modal>
